16fd16903b04110935e6cbc72d38c176f7e3e189
Sl3dge78 <45926471+Sl3dge78@users.noreply.github.com>
2021-06-20 01:26:36 +0200

    More logging to shader building

--- build_shaders.bat
+++ build_shaders.bat
@@ -2,15 +2,16 @@
 
 pushd resources\shaders\
 for %%v in (*.frag *.vert *.rchit *.rgen *.rmiss) do (
+    echo %%v
     "D:/VulkanSDK/1.2.162.0/Bin32/glslc.exe" "%%v" --target-env=vulkan1.2 -o "%%v".spv || goto :error
 )
 
 echo a > shaders.meta
 echo Shaders built successfully!
-goto :end
+popd
+exit -b 0
 
 :error
 echo Error(s) building shaders.
-
-:end
 popd
+exit -b 1
