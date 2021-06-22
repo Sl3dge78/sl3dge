abfc51e5e6fa3df13dbd18297ce38d8d3c2a974c
Sl3dge78 <45926471+Sl3dge78@users.noreply.github.com>
2021-06-22 20:12:33 +0200

    changed build method to loop through modules

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
 @echo off
-set project_name=win32
-set timestamp=%TIME:~3,2%%TIME:~6,2%
 
 set args= -GR- -EHa -nologo -Zi -experimental:external -external:anglebrackets -DDEBUG -Fotmp/ -Fdtmp/
 set include_path=-external:I D:\Guigui\Work\Prog\_include\ -external:I %VULKAN_SDK%\include
@@ -12,9 +10,22 @@ pushd tmp
 del *.pdb > NUL 2> NUL
 popd
 
-cl %args% -Fe%project_name% %include_path% %vulkan_include_path% src/win32.cpp %linker_options% -PDB:tmp/ -OUT:bin/win32.exe %libs%
-cl %args% -Fegame %include_path% %vulkan_include_path% -LD src/game.cpp %linker_options% -PDB:tmp/game_%timestamp%.pdb -IMPLIB:tmp/game.lib -OUT:bin/game.dll %libs%
-echo a > bin/game.meta
-cl %args% -Fevulkan %include_path% %vulkan_include_path% -LD src/vulkan.cpp %linker_options% -PDB:tmp/game_%timestamp%.pdb -IMPLIB:tmp/vulkan.lib -OUT:bin/vulkan.dll %libs%
-echo a > bin/vulkan.meta
+cl %args% -Fewin32 %include_path% %vulkan_include_path% src/win32.cpp %linker_options% -PDB:tmp/ -OUT:bin/win32.exe %libs%
+
+for %%G in (game,vulkan) do (
+    set timestamp=%TIME:~3,2%%TIME:~6,2%
+    cl %args% -Fe%%G %include_path% %vulkan_include_path% -LD src/%%G.cpp %linker_options% -PDB:tmp/%%G_%timestamp%.pdb -IMPLIB:tmp/%%G.lib -OUT:bin/%%G.dll %libs%
+    echo a > bin/%%G.meta
+
+)
+
+rem set timestamp=%TIME:~3,2%%TIME:~6,2%
+rem set module_name=game
+rem cl %args% -Fe%module_name% %include_path% %vulkan_include_path% -LD src/%module_name%.cpp %linker_options% -PDB:tmp/%module_name%_%timestamp%.pdb -IMPLIB:tmp/%module_name%.lib -OUT:bin/%module_name%.dll %libs%
+rem echo a > bin/%module_name%.meta
+
+rem set timestamp=%TIME:~3,2%%TIME:~6,2%
+rem set module_name=vulkan
+rem cl %args% -Fe%module_name% %include_path% %vulkan_include_path% -LD src/%module_name%.cpp %linker_options% -PDB:tmp/%module_name%_%timestamp%.pdb -IMPLIB:tmp/%module_name%.lib -OUT:bin/%module_name%.dll %libs%
+rem echo a > bin/%module_name%.meta
 echo Build completed!
