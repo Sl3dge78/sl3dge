0f078f218caf3cc3ac768b51e711fab3bdff25be
Sl3dge78 <45926471+Sl3dge78@users.noreply.github.com>
2021-07-01 19:06:18 +0200

    added tests for huffman encoding

--- src/tests/unit_test.c
+++ src/tests/unit_test.c
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+void TestHuffman() {
+    sLog("HUFFMAN");
+    u32 source[] = {3, 3, 3, 3, 3, 2, 4, 4};
+    u32 expected[] = {0b010, 0b011, 0b100, 0b101, 0b110, 0b00, 0b1110, 0b1111};
+    u32 out[ARRAY_SIZE(source)];
+    HuffmanCompute(ARRAY_SIZE(source), source, out);
+
+    for(u32 i = 0; i < ARRAY_SIZE(expected); ++i) {
+        TEST_EQUALS(out[i], expected[i], "%X");
+    }
+}
+
 void TestVec3() {
     {
         sLog("VEC3");
@@ -11,41 +23,39 @@ void TestVec3() {
         Vec3 test_2 = {1.0, 1.0, 1.0};
 
         Vec3 result = vec3_add(test_1, test_2);
-        TEST_EQUALS(result.x, test_1.x + test_2.x);
-        TEST_EQUALS(result.y, test_1.y + test_2.y);
-        TEST_EQUALS(result.z, test_1.z + test_2.z);
+        TEST_EQUALS(result.x, test_1.x + test_2.x, "%.2f");
+        TEST_EQUALS(result.y, test_1.y + test_2.y, "%.2f");
+        TEST_EQUALS(result.z, test_1.z + test_2.z, "%.2f");
 
         result = vec3_sub(test_1, test_2);
-        TEST_EQUALS(result.x, test_1.x - test_2.x);
-        TEST_EQUALS(result.y, test_1.y - test_2.y);
-        TEST_EQUALS(result.z, test_1.z - test_2.z);
+        TEST_EQUALS(result.x, test_1.x - test_2.x, "%.2f");
+        TEST_EQUALS(result.y, test_1.y - test_2.y, "%.2f");
+        TEST_EQUALS(result.z, test_1.z - test_2.z, "%.2f");
 
         f32 value = 4343949.4934939f;
         result = vec3_fmul(test_1, value);
-        TEST_EQUALS(result.x, test_1.x * value);
-        TEST_EQUALS(result.y, test_1.y * value);
-        TEST_EQUALS(result.z, test_1.z * value);
+        TEST_EQUALS(result.x, test_1.x * value, "%.2f");
+        TEST_EQUALS(result.y, test_1.y * value, "%.2f");
+        TEST_EQUALS(result.z, test_1.z * value, "%.2f");
 
         result = vec3_normalize(result);
         f32 length = vec3_length(result);
-        TEST_EQUALS(length, 1.0f);
+        TEST_EQUALS(length, 1.0f, "%.2f");
 
         Vec3 test_3 = {1.0f, 0.0f, 0.0f};
         length = vec3_length(test_3);
-        TEST_EQUALS(length, 1.0f);
+        TEST_EQUALS(length, 1.0f, "%.2f");
 
         result = vec3_fmul(test_3, 2.0f);
         length = vec3_length(result);
-        TEST_EQUALS(length, 2.0f);
+        TEST_EQUALS(length, 2.0f, "%.2f");
     }
 }
 
 int main(const int argc, const char *argv[]) {
     TEST_BEGIN();
-
-    u32 hex = 0xAABBCCDD;
-    u32 result = swap_u32(hex);
-    TEST_EQUALS(result, 0xDDCCBBAA);
+    TestVec3();
+    TestHuffman();
 
     TEST_END();
 
